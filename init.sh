4559987e411d2328f881cccb19577ccb8a40fef2
Ben Wilson <doubtingben@gmail.com>
2018-08-18 15:16:06 +0000

    initial version

---
+++ init.sh
@@ -0,0 +1,7 @@
+docker network create web
+mkdir -p /opt/traefik
+touch /opt/traefik/docker-compose.yml
+touch /opt/traefik/acme.json && chmod 600 /opt/traefik/acme.json
+touch /opt/traefik/traefik.toml
+
+cd /opt/traefik
